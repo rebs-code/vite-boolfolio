8710c0a9c74371d7bd6d5823c5f3b2c1ed922a3e
rebs-code <jack.rebussi@gmail.com>
2024-02-15 18:06:40 +0100

    create: AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppHeader',
+};
+</script>
+<template>
+    <header class="app-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-uppercase">My Portfolio</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<style scoped lang="scss"></style>
