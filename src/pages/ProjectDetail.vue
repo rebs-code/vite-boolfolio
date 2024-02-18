3d692cf18de08b1a0412eb6cb714bf4e555ce8b1
rebs-code <jack.rebussi@gmail.com>
2024-02-18 17:53:58 +0100

    add: API call to get project details in ProjectDetail.vue

--- src/pages/ProjectDetail.vue
+++ src/pages/ProjectDetail.vue
@@ -1,6 +1,35 @@
 <script>
+//import axios
+import axios from 'axios';
+
 export default {
     name: 'ProjectDetail',
+    data() {
+        return {
+            project: {},
+            //initialize the baseURL and apiUrls
+            baseURL: 'http://127.0.0.1:8000/',
+            apiUrls: {
+                'projects': 'api/projects',
+            }
+
+        };
+    },
+
+    methods: {
+        getProject() {
+            axios.get(this.baseURL + this.apiUrls.projects + '/' + this.$route.params.slug)
+                .then(response => {
+                    this.project = response.data;
+                })
+                .catch(error => {
+                    console.log('There was an error:', error);
+                });
+        }
+    },
+    created() {
+        this.getProject();
+    }
 };
 </script>
 <template>
