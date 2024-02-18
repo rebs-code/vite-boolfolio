0cafd2edd87639743a1b5525e69c24579cf3b00e
rebs-code <jack.rebussi@gmail.com>
2024-02-18 17:10:32 +0100

    add: content and back to home link to Notfound.vue

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -4,6 +4,8 @@ export default {
 };
 </script>
 <template>
-    <div>Not found</div>
+    <h2>Page Not Found</h2>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <router-link to="/">Go back to the home page</router-link>
 </template>
 <style lang="scss" scoped></style>
