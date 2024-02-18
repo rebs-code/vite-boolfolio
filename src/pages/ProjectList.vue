552699eddb6d6603e968883d645b63344582b204
rebs-code <jack.rebussi@gmail.com>
2024-02-18 13:03:01 +0100

    refactor: move AppMain to ProjectList

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -36,6 +36,28 @@ export default {
 };
 </script>
 <template>
-    <div>Project List</div>
+    <header class="app-main bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 p-3" v-for="project in projects" :key="project.id">
+                    <!-- bootstrap card for each projects -->
+                    <div class="card project-card">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-name">{{ project.name }}</h5>
+                            <p class="card-text">{{ project.description }}</p>
+                            <div class="d-flex mt-auto">
+                                <a href="/" class="btn btn-primary mt-auto mx-auto">View Project</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </header>
 </template>
-<style lang="scss" scoped></style>
+<style scoped lang="scss">
+.project-card {
+    min-height: 300px;
+}
+</style>
