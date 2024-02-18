068584a9f6e3c73d4959a1d495b24991110ae15b
rebs-code <jack.rebussi@gmail.com>
2024-02-18 17:08:18 +0100

    Add NotFound component to router.js

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import ProjectList from './pages/ProjectList.vue';
 import AppAbout from './pages/AppAbout.vue';
+import NotFound from './components/NotFound.vue';
 
 //create a new router instance
 const router = createRouter({
