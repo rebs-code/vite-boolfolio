68856766ff558bd602b31f014296d595d37ed768
rebs-code <jack.rebussi@gmail.com>
2024-02-20 12:01:07 +0100

    Refactor project detail page layout and add comment section

--- src/pages/ProjectDetail.vue
+++ src/pages/ProjectDetail.vue
@@ -31,8 +31,35 @@ export default {
 </script>
 <template>
     <div class="container">
-        <h2>{{ project.name }}</h2>
-        <p>{{ project.description }}</p>
+        <div>
+            <h2>{{ project.name }}</h2>
+            <p>{{ project.description }}</p>
+        </div>
+        <hr>
+        <!-- comment section -->
+        <section id="comments">
+            <h5>Comments</h5>
+            <ul v-if="project.comments.length">
+                <li v-for="comment in project.comments" :key="comment.id">
+                    <p>{{ comment.content }}</p>
+                </li>
+            </ul>
+            <!-- bootstrap alert if no comments -->
+            <div class="alert alert-info" role="alert" v-else>
+                No comments yet
+            </div>
+        </section>
+        <hr>
+        <section>
+            <h5>Add Comment</h5>
+            <!-- <form>
+                <div class="form-group">
+                    <label for="comment">Comment</label>
+                    <textarea class="form-control" id="comment" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form> -->
+        </section>
         <router-link to="/projects">Go back to projects</router-link>
     </div>
 </template>
